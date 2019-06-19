f9aa8736ce13a386b65267c4dad7956901d9c5a0
Axel Gómez <Axlngzh@gmail.com>
2019-06-19 12:15:11 -0400

    Agregando turtleSquare

---
+++ turtleSquare.py
@@ -0,0 +1,62 @@
+import turtle
+
+window=turtle.Screen()
+tortuga = turtle.Turtle()
+
+for i in range(6):
+    tortuga.forward(100)
+    tortuga.left(120)
+    
+
+    tortuga.forward(100)
+    tortuga.left(120)
+
+    tortuga.forward(100)
+    tortuga.left(180)
+
+    tortuga.forward(100)
+    tortuga.right(150)
+
+    tortuga.forward(87)
+    tortuga.left(150)
+
+    tortuga.forward(50)
+    tortuga.left(120)
+
+    tortuga.forward(50)
+    tortuga.left(120)
+
+    tortuga.forward(50)
+    tortuga.left(60)
+
+    tortuga.forward(50)
+    tortuga.right(60)
+
+tortuga.right(120)
+tortuga.forward(50)
+tortuga.left(30)
+
+for i in range(6):
+    
+    tortuga.forward(87)
+    tortuga.left(60)
+
+tortuga.forward(87)
+tortuga.left(90)
+
+tortuga.forward(150)
+
+for i in range(2):
+    tortuga.left(120)
+    tortuga.forward(150)
+
+tortuga.left(180)
+tortuga.forward(75)
+tortuga.right(120)
+
+for i in range(3):
+    
+    tortuga.forward(75)
+    tortuga.left(120)
+
+window.mainloop()
