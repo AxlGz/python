17ac2c10e95111940686f05f7536eb7a9935d41c
Axel Gómez <Axlngzh@gmail.com>
2019-06-19 21:10:05 -0400

    agregando variables

---
+++ practicas/operadores.py
@@ -0,0 +1,35 @@
+# OPERADORES MATEMÁTICOS BÁSICOS
+# Suma _+_: Solo suma
+2+2
+
+# Resta:
+2-6
+
+# Multiplicación:
+4*4
+
+# división: divide y uestra el resuldato con todos los decimales:
+3/2
+
+# ----____----____----____----____----____----____----____----____----____----____----____----____----____----____----____
+
+# OPERADORES RAROS: 
+
+# //: Es división de entero, básicamente tiramos la parte decimal
+3//2
+
+# %: Es el residuo de la división, lo que te sobra.
+4%2
+
+# **: Exponente
+4**2
+
+
+# Estos  reasignan el valor de una variable:
+a+=5 #le agrega 5 más lo que tenía antes la variable.
+a-=5 #le quita 5 a lo que tenía antes la variable.
+a*=5# multiplica por 5 lo que tenía la variable.
+a/=5#Divide lo que tenía entre 5, mostrando los decimales.
+a//=5#Divide lo que tenía entre 5, SIN los decimales.
+a%=5#divide y reemplaza el contenido de la variable con el módulo de la variable.
+a**=5#toma el valor de una variable y lo eleva a la 5ta potencia (reemplaza el valor de la variable)
---
+++ practicas/variablesYExpresiones.py
@@ -0,0 +1,31 @@
+#  Asignación de valor y tipos de variables
+# A las variables se le puede asignar un valor con _=_.
+
+# Variable común:
+message = 'how are you?'
+
+# variable privada:
+_age = 20
+
+#Constante:
+PI= 3.14159
+
+#Variable super privada y no se debe tocar:
+__noTocar: 'por favor no me modifiques'
+
+#Reglas para crear nombres de variables:
+# *Pueden contener números y letras.
+# *NO pueden empezar con números.
+# *Multiples palabras se unen con _
+# *No podemos usar palabras reservadas de Python
+
+#----____----____----____----____----____----____----____----____----____----____----____----____----____----____----____----____---
+
+#Expresiones:
+# Una expresión (expression) es una combinación de valores, variables y operadores (Evaluan y muestran un output.)
+# Un enunciado (statement) es una unidad de código que tiene un efecto (por ejemplo la creacion de una variable.)
+
+#----____----____----____----____----____----____----____----____----____----____----____----____----____----____----____----____---
+
+# Orden de operaciones
+# Paréntesis > Exponentes > Multiplicación > División > Adición > Substracción = PEMDAS
